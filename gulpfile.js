2191b34ee1dd0645d23295261be75d9afc13ce38
Diego Rivera <167586944+dart94@users.noreply.github.com>
2024-05-17 16:09:15 -0700

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+//Javascript
+const terser = require('gulp-terser-js')
+
+
+function css(done) {
+    src('src/scss/**/*.scss')// Identificar el archivo el archivo SASS
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(sass())// Compilar 
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest("build/css"));// }Almacenarlo en el disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final
+
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function javascript(done) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
+}
+
+function dev(done) {
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', javascript)
+    done();
+}
+
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
